edbdce8293e312e7cd4989cc5aaf753426a15406
Uğur Özyılmazel <vigo@devchain.network>
2025-01-04 22:17:07 +0300

    wip

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.23.4
 
 require (
 	github.com/go-playground/webhooks/v6 v6.4.0
+	github.com/google/uuid v1.6.0
 	github.com/kylelemons/godebug v1.1.0
 	github.com/valyala/fasthttp v1.58.0
 	github.com/vigo/getenv v0.0.1
--- internal/apiserver/handlers.go
+++ internal/apiserver/handlers.go
@@ -3,8 +3,10 @@ package apiserver
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/go-playground/webhooks/v6/github"
+	"github.com/google/uuid"
 	"github.com/kylelemons/godebug/pretty"
 	"github.com/valyala/fasthttp"
 	"github.com/valyala/fasthttp/fasthttpadaptor"
@@ -16,6 +18,48 @@ func healthCheckHandler(ctx *fasthttp.RequestCtx) {
 	ctx.SetBodyString("OK")
 }
 
+// AnythingUnknown ...
+const AnythingUnknown = "unknown"
+
+// GitHubWebhookRequestHeaders ...
+type GitHubWebhookRequestHeaders struct {
+	Event      string
+	TargetType string
+	DeliveryID uuid.UUID
+	HookID     uint64
+	TargetID   uint64
+}
+
+// ParseGitHubHTTPHeaders ...
+func ParseGitHubHTTPHeaders(h http.Header) *GitHubWebhookRequestHeaders {
+	out := &GitHubWebhookRequestHeaders{
+		Event:      AnythingUnknown,
+		TargetType: AnythingUnknown,
+	}
+
+	if val := h.Get("X-Github-Event"); val != "" {
+		out.Event = val
+	}
+
+	if val, err := uuid.Parse(h.Get("X-Github-Delivery")); err == nil {
+		out.DeliveryID = val
+	}
+
+	if val, err := strconv.ParseUint(h.Get("X-Github-Hook-Id"), 10, 64); err == nil {
+		out.HookID = val
+	}
+
+	if val, err := strconv.ParseUint(h.Get("X-Github-Hook-Installation-Target-Id"), 10, 64); err == nil {
+		out.TargetID = val
+	}
+
+	if val := h.Get("X-Github-Hook-Installation-Target-Type"); val != "" {
+		out.TargetType = val
+	}
+
+	return out
+}
+
 func githubWebhookHandler(opts *githubHandlerOptions) fasthttp.RequestHandler {
 	return func(ctx *fasthttp.RequestCtx) {
 		var httpReq http.Request
@@ -24,6 +68,9 @@ func githubWebhookHandler(opts *githubHandlerOptions) fasthttp.RequestHandler {
 
 			return
 		}
+
+		httpHeaders := ParseGitHubHTTPHeaders(httpReq.Header)
+		pretty.Print(httpHeaders)
 
 		listenEvents := []github.Event{
 			github.IssuesEvent,
