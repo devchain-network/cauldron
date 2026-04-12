ed9ec49d488edb02a50f878826f5bd8fc3997149
Uğur Özyılmazel <vigo@devchain.network>
2026-04-12 22:35:37 +0300

    add gitlab org settings migration (#78)
    * add gitlab org settings migration
    * fix group_name field length

---
+++ migrations/000017_gitlab_org_settings.down.sql
@@ -0,0 +1,1 @@
+DROP TABLE IF EXISTS "cauldron"."gitlab_org_settings";
---
+++ migrations/000017_gitlab_org_settings.up.sql
@@ -0,0 +1,15 @@
+--
+-- create "gitlab_org_settings" table
+--
+CREATE TABLE "cauldron"."gitlab_org_settings" (
+    "id" SERIAL PRIMARY KEY,
+    "uid" UUID DEFAULT uuid_generate_v4(),
+    "group_name" VARCHAR(255) UNIQUE NOT NULL,
+    "created_at" TIMESTAMP NOT NULL DEFAULT (NOW() AT TIME ZONE 'UTC'),
+    "admin_user_id" BIGINT REFERENCES "cauldron"."app_user"(id),
+    "repo_mode" VARCHAR(10) NOT NULL DEFAULT 'all' CHECK (repo_mode IN ('all', 'selected')),
+    "selected_repos" JSONB NOT NULL DEFAULT '[]'::jsonb,
+    "meta_data" JSONB NOT NULL DEFAULT '{}'::jsonb
+);
+
+CREATE INDEX "idx_gitlab_org_settings_meta_data" ON "cauldron"."gitlab_org_settings" USING gin ("meta_data");
